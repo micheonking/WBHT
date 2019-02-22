e246e732cb8d75612b4432feb5e252b06b8c8136
miCheon <miCheon@k-fs.co.kr>
2019-02-22 16:36:12 +0900

    Confimer

--- application/src/main/java/myApp/client/vi/sys/Sys00_Admin.java
+++ application/src/main/java/myApp/client/vi/sys/Sys00_Admin.java
@@ -1,7 +1,9 @@
 package myApp.client.vi.sys;
 
+import com.google.gwt.safehtml.shared.SafeHtml;
+import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 import com.sencha.gxt.widget.core.client.ContentPanel;
-import com.sencha.gxt.widget.core.client.PlainTabPanel;
+import com.sencha.gxt.widget.core.client.TabPanel;
 import com.sencha.gxt.widget.core.client.container.MarginData;
 
 import myApp.client.vi.hom.bullentin.AdminBoard;
@@ -12,8 +14,11 @@ public class Sys00_Admin extends ContentPanel {
 
 		this.setHeading("시스템 관리자");
 		this.setBorders(true);
+		SafeHtml button1Html = SafeHtmlUtils.fromTrustedString(
+				"<div style='border: 1px solid white; padding: 15px; height: auto; '><font color='#ffffff' style='font-size:20px;font-weight:bold'>시스템 관리자</font></div>");
 
-		PlainTabPanel tabPanel = new PlainTabPanel();
+		this.setHeading(button1Html);
+		TabPanel tabPanel = new TabPanel();
 
 		AdminBoard tabAdmin = new AdminBoard();
 		tabPanel.add(tabAdmin, "게시판관리");
